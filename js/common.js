bcbecb17df77fdbec1e93397ede841c588ad8abf
bernard_jung <bernardjung88@gmail.com>
2024-01-06 00:12:22 +0900

    Start project

---
+++ js/common.js
@@ -0,0 +1,15 @@
+// full page
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true,
+        navigation: true,
+        slidesNavigation: true,
+        navigationTooltips: [ 'MAIN', 'PORTFOLIO', 'CONTACT' ],
+        showActiveTooltip: true,
+    });
+});
+
+
+
